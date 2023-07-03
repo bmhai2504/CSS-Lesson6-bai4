9e01343d074c062c463bbe166f5bdb2b00f8b0af
bmhai2504 <bmhai2504@gmail.com>
2023-07-03 16:33:18 +0700

    finish

---
+++ script.js
@@ -0,0 +1,17 @@
+let ddts = document.getElementById('ddts');
+ddts.addEventListener('click', () => {
+      let ddts_txt = document.getElementById('ddts_txt');
+      window.scrollTo(0, ddts_txt.offsetTop);
+});
+
+let kcnts = document.getElementById('kcnts');
+kcnts.addEventListener('click', () => {
+      let kcnts_txt = document.getElementById('kcnts_txt');
+      window.scrollTo(0, kcnts_txt.offsetTop);
+});
+
+let abhtt = document.getElementById('abhtt');
+abhtt.addEventListener('click', () => {
+      let abhtt_txt = document.getElementById('abhtt_txt');
+      window.scrollTo(0, abhtt_txt.offsetTop);
+});
